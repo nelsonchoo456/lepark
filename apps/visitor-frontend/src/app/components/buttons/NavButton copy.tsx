64ea87726db61509c6e4712139f8357ec478b800
micaellaa <99943347+micaellaa@users.noreply.github.com>
2024-08-30 00:24:17 +0800

    visitor-frontend: MainLanding improved ui

--- apps/visitor-frontend/src/app/components/buttons/NavButton copy.tsx
+++ apps/visitor-frontend/src/app/components/buttons/NavButton copy.tsx
@@ -15,14 +15,14 @@ export const NavButton = React.forwardRef<HTMLButtonElement, ButtonProps>(
       <button
       // flex-1 bg-green-50
         // style={{ height: "4rem" }}
-        className={`flex flex-1 flex-col items-center cursor-pointer transition-all duration-200 rounded hover:text-green-500
-          md:py-2 md:border-r md:hover:bg-highlightGreen-50`}
+        className={`flex flex-col items-center cursor-pointer transition-all duration-200 rounded hover:text-green-500 
+          md:p-2 flex-1 md:bg-highlightGreen-100 md:hover:bg-green-100 md:flex-row`}
         onClick={onClick}
         {...fields}
       >
         <div className={`h-14 w-14 rounded-full flex justify-center items-center text-3xl cursor-pointer
           text-highlightGreen-400 font-semibold bg-highlightGreen-100 hover:bg-green-100 transition-all duration-200
-          md:h-10 md:bg-transparent md:hover:bg-transparent`}>
+          md:h-12 md:w-full md:bg-transparent md:rounded `}>
           {icon}
         </div>
         <div className='text-sm'>
