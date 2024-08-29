8e53b5ba37d6afa8d7880afcac5a006c16da0ec1
micaellaa <99943347+micaellaa@users.noreply.github.com>
2024-08-30 00:45:17 +0800

    Set up IOS on Capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.leparks.visitor',
   appName: 'visitor-frontend',
-  webDir: 'dist'
+  webDir: 'dist/apps/visitor-frontend', // added by Mic
+  bundledWebRuntime: false, // added by Mic
 };
 
 export default config;
