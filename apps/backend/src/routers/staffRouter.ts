ae4c632f5f5822660db32780c8a43f633481676f
Nelson Choo <nelsonchoo456@gmail.com>
2024-08-31 16:42:00 +0800

    forgot, reset password, login, logout for staff

--- apps/backend/src/routers/staffRouter.ts
+++ apps/backend/src/routers/staffRouter.ts
@@ -37,7 +37,10 @@ router.put('/updateStaffDetails/:id', async (req, res) => {
     const staffId = req.params.id;
     const updateData: Prisma.StaffUpdateInput = req.body;
 
-    const updatedStaff = await StaffService.updateStaffDetails(staffId, updateData);
+    const updatedStaff = await StaffService.updateStaffDetails(
+      staffId,
+      updateData,
+    );
     res.status(200).json(updatedStaff);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -78,6 +81,54 @@ router.put('/updateStaffIsActive/:id', async (req, res) => {
       requesterId,
     );
     res.status(200).json(updatedStaff);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { token, user } = await StaffService.login(req.body);
+
+    res.cookie('jwtToken_Staff', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production', // secure in production
+      sameSite: 'strict',
+      maxAge: 4 * 60 * 60 * 1000, // 4 hours (needs to be same expiry as the JWT token)
+    });
+
+    res.status(200).json(user); // send user data in the response body
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.post('/logout', (_, res) => {
+  res
+    .clearCookie('jwtToken_Staff', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production', // secure in production
+      sameSite: 'strict',
+    })
+    .status(200)
+    .send({ message: 'Logout successful' });
+});
+
+router.post('/forgot-password', async (req, res) => {
+  try {
+    const { email } = req.body;
+    await StaffService.requestPasswordReset(email);
+    res.status(200).json({ message: 'Password reset email sent successfully' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.post('/reset-password', async (req, res) => {
+  try {
+    const { token, newPassword } = req.body;
+    await StaffService.resetPassword(token, newPassword);
+    res.status(200).json({ message: 'Password reset successfully' });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
