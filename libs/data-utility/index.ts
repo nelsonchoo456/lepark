30c938a94b5797483ee686c54ffcba7a0ff66abe
vveining <yongweining.ywn@gmail.com>
2024-09-03 13:45:39 +0800

    feature:libs setup data-utility for easy access of dropdown items, specifically for species form

---
+++ libs/data-utility/index.ts
@@ -0,0 +1,4 @@
+export * from './speciesData';
+// Export from other files in data-utility as needed
+// export * from './otherFile1';
+// export * from './otherFile2';
---
+++ libs/data-utility/speciesData.js
@@ -0,0 +1,16 @@
+//create species form
+export const phylums = [
+  "Anthocerotophyta", "Bryophyta", "Charophyta", "Chlorophyta", "Cycadophyta",
+  "Ginkgophyta", "Glaucophyta", "Gnetophyta", "Lycopodiophyta", "Lycophyta",
+  "Magnoliophyta", "Marchantiophyta", "Hepatophyta", "Polypodiophyta",
+  "Monilophyta", "Picozoa", "Pinophyta", "Coniferophyta", "Prasinodermophyta",
+  "Rhodophyta"
+];
+
+export const regions = [
+  'North America', 'Central America', 'South America', 'Caribbean', 'Europe', 'Northern Africa',
+  'Sub-Saharan Africa', 'Middle East', 'Central Asia', 'South Asia', 'Southeast Asia',
+  'East Asia', 'Australasia', 'Pacific Islands', 'Mediterranean', 'Western Asia', 'Eastern Asia',
+  'Southern Africa', 'Arctic', 'Antarctica', 'Tropical Rainforest', 'Temperate Forest',
+  'Savanna', 'Desert', 'Tundra', 'Boreal Forest'
+];
