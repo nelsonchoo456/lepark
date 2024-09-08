81ea567326cf4b0454c4525c88bb6d416f3cb1b4
ely <96276919+elysiaojr@users.noreply.github.com>
2024-09-08 15:42:34 +0800

    visitor axios

---
+++ libs/data-access/src/lib/api/visitor.ts
@@ -0,0 +1,100 @@
+import axios, { AxiosResponse } from 'axios';
+import {
+    RegisterVisitorData,
+    VisitorResponse,
+    VisitorLoginData,
+    VisitorLogoutResponse,
+    VisitorUpdateData,
+    VisitorPasswordResetRequestData,
+    VisitorPasswordResetData,
+} from '../types/visitor';
+import client from './client';
+
+const URL = '/visitors';
+
+// export async function registerVisitor(data: RegisterVisitorData): Promise<AxiosResponse<VisitorResponse>> {
+
+// }
+
+export async function getAllVisitors(): Promise<AxiosResponse<VisitorResponse[]>> {
+  try {
+    const response: AxiosResponse<VisitorResponse[]> = await client.get(`${URL}/getAllVisitors`);
+    return response;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw error.response?.data.error || error.message;
+    } else {
+      throw error;
+    }
+  }
+}
+
+export async function viewVisitorDetails(id: string): Promise<AxiosResponse<VisitorResponse>> {
+  try {
+    const response: AxiosResponse<VisitorResponse> = await client.get(`${URL}/viewVisitorDetails/${id}`);
+    return response;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw error.response?.data.error || error.message;
+    } else {
+      throw error;
+    }
+  }
+}
+
+export async function updateVisitorDetails(id: string, data: VisitorUpdateData): Promise<AxiosResponse<VisitorResponse>> {
+  try {
+    const response: AxiosResponse<VisitorResponse> = await client.put(`${URL}/updateVisitorDetails/${id}`, data);
+    return response;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw error.response?.data.error || error.message;
+    } else {
+      throw error;
+    }
+  }
+}
+
+// export async function loginVisitor(data: VisitorLoginData): Promise<AxiosResponse<VisitorResponse>> {
+//   try {
+//     const response: AxiosResponse<VisitorResponse> = await client.post(`${URL}/login`, data);
+//     return response;
+//   } catch (error) {
+//     if (axios.isAxiosError(error)) {
+//       throw error.response?.data.error || error.message;
+//     } else {
+//       throw error;
+//     }
+//   }
+// }
+
+// export async function logoutVisitor(): Promise<AxiosResponse<VisitorLogoutResponse>> {
+//   return client.post(`${URL}/logout`);
+// }
+
+// export async function forgotPassword(data: VisitorPasswordResetRequestData): Promise<AxiosResponse<{ message: string }>> {
+//   try {
+//     const response: AxiosResponse<{ message: string }> = await client.post(`${URL}/forgot-password`, data);
+//     return response;
+//   } catch (error) {
+//     if (axios.isAxiosError(error)) {
+//       throw error.response?.data.error || error.message;
+//     } else {
+//       throw error;
+//     }
+//   }
+// }
+
+// // Reset Password
+// export async function resetPassword(data: VisitorPasswordResetData): Promise<AxiosResponse<{ message: string }>> {
+//   try {
+//     const response: AxiosResponse<{ message: string }> = await client.post(`${URL}/reset-password`, data);
+//     return response;
+//   } catch (error) {
+//     if (axios.isAxiosError(error)) {
+//       throw error.response?.data.error || error.message;
+//     } else {
+//       throw error;
+//     }
+//   }
+// }
