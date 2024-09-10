d03e8c63b77fedfe01dde548b5700681be2418df
micaellaa <99943347+micaellaa@users.noreply.github.com>
2024-09-10 19:36:45 +0800

    Create Zone

---
+++ apps/backend/src/routers/zoneRouter.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import ZoneService from '../services/ZoneService';
+
+const router = express.Router();
+
+router.post('/createZone', async (req, res) => {
+  try {
+    const park = await ZoneService.createZone(req.body);
+    res.status(201).json(park);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.get('/getAllZones', async (_, res) => {
+  try {
+    const zonesList = await ZoneService.getAllZones();
+    res.status(200).json(zonesList);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+export default router;
---
+++ apps/backend/src/schemas/zoneSchema.ts
@@ -0,0 +1,22 @@
+export interface ZoneCreateData {
+  name: string;
+  description?: string;
+  openingHours: Date[];
+  closingHours: Date[];
+  geom?: string;
+  paths?: string;
+  zoneStatus: 'OPEN' | 'CLOSED' | 'UNDER_CONSTRUCTION' | 'LIMITED_ACCESS';
+  parkId: number;
+}
+
+export interface ZoneResponseData {
+  id: number;
+  name: string;
+  description?: string;
+  openingHours: Date[];
+  closingHours: Date[];
+  geom?: string;
+  paths?: string;
+  zoneStatus: 'OPEN' | 'CLOSED' | 'UNDER_CONSTRUCTION' | 'LIMITED_ACCESS';
+  parkId: string;
+}
