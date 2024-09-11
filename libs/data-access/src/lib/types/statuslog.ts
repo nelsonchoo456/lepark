79bc273e2b9202c76b87477be6b0bb66bba80790
Aaron Foo <74005160+aaronfyr@users.noreply.github.com>
2024-09-11 09:48:26 +0800

    fix/index.ts renaming of types

--- libs/data-access/src/lib/types/statuslog.ts
+++ libs/data-access/src/lib/types/statuslog.ts
@@ -1,34 +1,28 @@
-export enum OccurrenceStatusEnum {
-    HEALTHY = 'HEALTHY',
-    MONITOR_AFTER_TREATMENT = 'MONITOR_AFTER_TREATMENT',
-    NEEDS_ATTENTION = 'NEEDS_ATTENTION',
-    URGENT_ACTION_REQUIRED = 'URGENT_ACTION_REQUIRED',
-    REMOVED = 'REMOVED',
-  }
-  
-  export interface StatusLogData {
-    name: string;
-    description: string;
-    dateCreated: string;
-    images?: string[];
-    statusLogType: OccurrenceStatusEnum;
-    occurrenceId: string;
-  }
-  
-  export interface StatusLogResponse {
-    id: string;
-    name: string;
-    description: string;
-    dateCreated: string;
-    images: string[];
-    statusLogType: OccurrenceStatusEnum;
-    occurrenceId: string;
-  }
-  
-  export interface StatusLogUpdateData {
-    name?: string;
-    description?: string;
-    dateCreated?: string;
-    images?: string[];
-    statusLogType?: OccurrenceStatusEnum;
-  }
+import { OccurrenceStatusEnum } from './occurrence';
+
+export interface StatusLogData {
+  name: string;
+  description: string;
+  dateCreated: string;
+  images?: string[];
+  statusLogType: OccurrenceStatusEnum;
+  occurrenceId: string;
+}
+
+export interface StatusLogResponse {
+  id: string;
+  name: string;
+  description: string;
+  dateCreated: string;
+  images: string[];
+  statusLogType: OccurrenceStatusEnum;
+  occurrenceId: string;
+}
+
+export interface StatusLogUpdateData {
+  name?: string;
+  description?: string;
+  dateCreated?: string;
+  images?: string[];
+  statusLogType?: OccurrenceStatusEnum;
+}
