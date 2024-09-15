3602cf3fac4322c04e3c66e0708ff892a3fdcb00
micaellaa <99943347+micaellaa@users.noreply.github.com>
2024-09-15 16:03:47 +0800

    staffFrontend: occurrence - species tab
    added taxonomy tree

--- apps/staff-frontend/src/app/pages/OccurrenceDetails/components/AboutTab.tsx
+++ apps/staff-frontend/src/app/pages/OccurrenceDetails/components/AboutTab.tsx
@@ -1,35 +1,145 @@
 import { LogoText } from '@lepark/common-ui';
-import { Card, Descriptions, Divider } from 'antd';
+import { Button, Card, Descriptions, Divider, Tree, Typography } from 'antd';
 import React from 'react';
-import { OccurrenceResponse } from '@lepark/data-access';
+import { OccurrenceResponse, SpeciesResponse } from '@lepark/data-access';
+import { MdArrowDownward, MdArrowOutward } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
 
-const AboutTab = ({ occurrence }: any) => {
-  const descriptionsItems = Object.entries(occurrence).map(([key, val]) => ({
-    key,
-    label: key,
-    children: 'keke',
-  }));
+interface AboutTabProps {
+  species?: SpeciesResponse;
+  occurrence?: OccurrenceResponse;
+}
+const AboutTab = ({ species, occurrence }: AboutTabProps) => {
+  const navigate = useNavigate();
+
+  // Tree data
+  const treeData = [
+    {
+      title: (
+        <>
+          <div className="font-bold">{species?.phylum}</div>
+          <div className="text-xs italic opacity-50">Phylum</div>
+        </>
+      ),
+      key: '0',
+      children: [
+        {
+          title: (
+            <>
+              <div className="font-bold">{species?.class}</div>
+              <div className="text-xs italic opacity-50">Class</div>
+            </>
+          ),
+          key: '0-0',
+          children: [
+            {
+              title: (
+                <>
+                  <div className="font-bold">{species?.order}</div>
+                  <div className="text-xs italic opacity-50">Order</div>
+                </>
+              ),
+              key: '0-0-0',
+              children: [
+                {
+                  title: (
+                    <>
+                      <div className="font-bold">{species?.family}</div>
+                      <div className="text-xs italic opacity-50">Family</div>
+                    </>
+                  ),
+                  key: '0-0-0-0',
+                  children: [
+                    {
+                      title: (
+                        <>
+                          <div className="font-bold">{species?.genus}</div>
+                          <div className="text-xs italic opacity-50">Genus</div>
+                        </>
+                      ),
+                      key: '0-0-0-0-0',
+                      children: [
+                        {
+                          title: (
+                            <>
+                              <LogoText className="font-bold" onClick={() => navigate(`/species/${species?.id}`)}>{species?.speciesName}</LogoText>
+                              <div className="text-xs italic opacity-50">Species</div>
+                            </>
+                          ),
+                          key: '0-0-0-0-0-0',
+                          children: [
+                            {
+                              title: (
+                                <div className="py-2 px-4 rounded bg-green-50">
+                                  <LogoText className="font-bold">{occurrence?.title}</LogoText>
+                                  <div className="text-xs opacity-50">This Occurrence</div>
+                                </div>
+                              ),
+                              key: '0-0-0-0-0-0-0',
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ];
+
   return (
-    <div className="flex gap-4">
-      <Card className="flex-1 p-4 cursor-pointer hover:bg-green-50 transition-3s" styles={{ body: { padding: 0 } }}>
-        <div className="flex gap-4">
-          <div
-            style={{
-              backgroundImage: `url('https://www.travelbreatherepeat.com/wp-content/uploads/2020/03/Singapore_Orchids_Purple.jpg')`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-              color: 'white',
-              overflow: 'hidden',
-            }}
-            className="h-20 w-20 rounded-full shadow-lg p-4"
-          />
-          <div className="w-64">
-            <div className='text-lg font-bold'>Species</div>
-            <LogoText>Orchid</LogoText>
+    <div>
+      {/* cursor-pointer hover:bg-green-50 transition-3s */}
+      <Card className="flex-1 p-4 mb-4" styles={{ body: { padding: 0 } }}>
+        <div className="flex gap-2">
+          {species?.images && species.images.length > 0 ? (
+            <div
+              style={{
+                backgroundImage: `url('${species.images[0]}')`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                overflow: 'hidden',
+              }}
+              className="h-20 w-20 rounded-full bg-gray-200 shadow-lg p-4 grow-0"
+            />
+          ) : (
+            <div className="h-20 w-20 rounded-full bg-gray-200 shadow-lg p-4" />
+          )}
+          <div className="w-full">
+            {/* <div className='text-lg font-bold'>Species</div> */}
+            <div
+              className="cursor-pointer hover:bg-green-50/40 transition-3s p-2 px-4 rounded-full flex items-center justify-between"
+              onClick={() => navigate(`/species/${species?.id}`)}
+            >
+              <div>
+                <LogoText className="text-lg">{species?.commonName}</LogoText>
+                <LogoText className="text-base italic opacity-50 ml-4">{species?.speciesName}</LogoText>
+              </div>
+              <Button shape="circle" icon={<MdArrowOutward />}></Button>
+            </div>
+            <Typography.Paragraph
+              ellipsis={{
+                rows: 3,
+                expandable: true,
+                symbol: 'more',
+              }}
+              className='pl-4'
+            >
+              {species?.speciesDescription}
+            </Typography.Paragraph>
           </div>
         </div>
       </Card>
-      <Card className="flex-1" styles={{ body: { padding: 0 } }}></Card>
+      <Card className='w-full' styles={{ body: {padding: 0} }}>
+        <Divider className='w-full' orientation="left"><LogoText className="text-base italic">Taxonomy Tree</LogoText></Divider>
+        <div className='flex justify-center mb-2'>
+          <Tree treeData={treeData} defaultExpandAll showLine/>
+        </div>
+      </Card>
     </div>
   );
 };
