d53b9db915546822ac1f83aac4d308a8b3de4798
alantay11 <alantay1111@gmail.com>
2024-09-15 13:24:18 +0800

    add searchbar to occurrence list inside species

--- apps/visitor-frontend/src/app/pages/Taxonomy/components/OccurrencesTab.tsx
+++ apps/visitor-frontend/src/app/pages/Taxonomy/components/OccurrencesTab.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import OccurrenceTable from './OccurrenceTable';
 import { SpeciesResponse } from '@lepark/data-access';
+import { FiSearch } from 'react-icons/fi';
+import { Input } from 'antd';
 
 interface OccurrencesTabProps {
   species: SpeciesResponse; // Define the type for species
@@ -11,6 +13,17 @@ const OccurrencesTab: React.FC<OccurrencesTabProps> = ({ species }) => {
 
   return (
     <div>
+      <Input
+        suffix={<FiSearch />}
+        placeholder="Search in Occurrences..."
+        className="mb-4 bg-white"
+        style={{
+          outline: '2px solid rgba(248, 248, 248, var(--tw-bg-opacity))', // Add outline with desired color
+          borderRadius: '4px', // Optional: Add border radius
+        }}
+        variant="filled"
+      />
+
       <OccurrenceTable speciesId={species.id} loading={loading} />
     </div>
   );
