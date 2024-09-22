895ad937e1e6d31dc3302a126c9f03a2d13fd1c7
ely <96276919+elysiaojr@users.noreply.github.com>
2024-09-22 17:27:29 +0800

    ui: changed create staff to match the other create pages

--- apps/staff-frontend/src/app/pages/StaffManagement/CreateStaff.tsx
+++ apps/staff-frontend/src/app/pages/StaffManagement/CreateStaff.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Form, Input, Button, Select, Descriptions, Switch, Space, message, Typography, notification, Card, Divider } from 'antd';
+import { Form, Input, Button, Select, Descriptions, Switch, Space, message, Typography, notification, Card, Divider, Flex, Result, Steps } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import PageHeader from '../../components/main/PageHeader';
 import { ContentWrapperDark, useAuth } from '@lepark/common-ui';
 import { registerStaff, RegisterStaffData, StaffResponse, StaffType, ParkResponse, getParkById, getAllParks } from '@lepark/data-access';
 import PageHeader2 from '../../components/main/PageHeader2';
-import crypto from 'crypto';
+import CreateDetailsStep from './components/CreateDetailsStep';
 
 const { Option } = Select;
 
@@ -16,9 +16,13 @@ const CreateStaff: React.FC = () => {
   const [parks, setParks] = useState<ParkResponse[]>([]);
   const { Text } = Typography;
   const notificationShown = useRef(false);
+  const [currStep, setCurrStep] = useState<number>(0);
+  const [messageApi, contextHolder] = message.useMessage();
+  const [createdData, setCreatedData] = useState<StaffResponse | null>(null);
 
-  const onFinish = async (values: any) => {
+  const handleSubmit = async () => {
     try {
+      const values = await form.validateFields();
       const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
       const generatedPassword = Array.from(window.crypto.getRandomValues(new Uint32Array(10)))
         .map((value) => characters[value % characters.length])
@@ -35,19 +39,17 @@ const CreateStaff: React.FC = () => {
         isFirstLogin: true,
       };
 
-      // console.log('Received values of form:', newStaffDetails);
       const response = await registerStaff(newStaffDetails);
-      // console.log('Success:', response.data);
-      message.success('Staff added successfully!');
-      // Navigate to another page or show success message
-      navigate('/staff-management');
+      setCurrStep(1);
+      setCreatedData(response.data);
+      messageApi.success('Staff added successfully!');
     } catch (error: any) {
       console.error(error);
       const errorMessage = error.message || error.toString();
       if (errorMessage.includes('Invalid email address')) {
-        message.error('Invalid email format.');
+        messageApi.error('Invalid email format.');
       } else {
-        message.error(errorMessage || 'Failed to update staff details.');
+        messageApi.error(errorMessage || 'Failed to update staff details.');
       }
     }
   };
@@ -63,7 +65,6 @@ const CreateStaff: React.FC = () => {
       }
       navigate('/');
     } else {
-      // Fetch parks data from the database
       getAllParks()
         .then((response) => {
           setParks(response.data);
@@ -72,7 +73,7 @@ const CreateStaff: React.FC = () => {
           console.error('There was an error fetching the parks data!', error);
         });
     }
-  }, [user]);
+  }, [user, navigate]);
 
   const getParkName = (parkId?: number) => {
     const park = parks.find((park) => park.id === parkId);
@@ -92,102 +93,69 @@ const CreateStaff: React.FC = () => {
     },
   ];
 
+  const content = [
+    {
+      key: 'details',
+      children: (
+        <CreateDetailsStep
+          form={form}
+          parks={parks}
+          user={user}
+        />
+      ),
+    },
+    {
+      key: 'complete',
+      children: <>Created</>,
+    },
+  ];
+
+  if (user?.role !== StaffType.SUPERADMIN && user?.role !== StaffType.MANAGER) {
+    return <></>;
+  }
+
   return (
     <ContentWrapperDark>
       <PageHeader2 breadcrumbItems={breadcrumbItems} />
       <Card>
-        <Form
-          form={form}
-          name="create_staff"
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          // style={{ maxWidth: 50 }}
-          className="max-w-[600px] mx-auto mt-8"
-          autoComplete="off"
-          labelCol={{ span: 8 }}
-        >
-          <Divider orientation="left">Staff Details</Divider>
-          <Form.Item name="firstNameInput" label="First Name" rules={[{ required: true, message: 'Please enter a first name.' }]}>
-            <Input />
-          </Form.Item>
-          <Form.Item name="lastNameInput" label="Last Name" rules={[{ required: true, message: 'Please enter a last name.' }]}>
-            <Input />
-          </Form.Item>
-          <Form.Item name="roleSelect" label="Role" rules={[{ required: true, message: 'Please select a role.' }]}>
-            <Select placeholder="Select a Role" allowClear>
-              {Object.values(StaffType)
-                .filter((role) => {
-                  if (user?.role === StaffType.MANAGER) {
-                    return role !== StaffType.MANAGER && role !== StaffType.SUPERADMIN;
-                  } else if (user?.role === StaffType.SUPERADMIN) {
-                    return role !== StaffType.SUPERADMIN; //removed option to create SUPERADMIN; we assume SUPERADMINs are created in system
-                  }
-                  return true;
-                })
-                .map((role) => (
-                  <Select.Option key={role} value={role}>
-                    {role.replace(/_/g, ' ')}
-                  </Select.Option>
-                ))}
-            </Select>
-          </Form.Item>
-          <Form.Item
-            name="emailInput"
-            label="Email"
-            rules={[
-              { required: true, message: 'Please enter an email.' },
-              { type: 'email', message: 'Please enter a valid email.' },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          {/* <Form.Item name="passwordInput" label="Password" rules={[
-          { required: true, message: 'Please enter a password.' },
-          { pattern: /^.{8,}$/, message: 'Password must be at least 8 characters long.' }
-        ]}>
-          <Input.Password />
-        </Form.Item> */}
-          <Form.Item
-            name="contactNumberInput"
-            label="Contact Number"
-            rules={[
-              { required: true, message: 'Please enter a contact number.' },
-              {
-                pattern: /^[689]\d{7}$/,
-                message: 'Contact number must consist of exactly 8 digits and be a valid Singapore contact number',
-              },
-            ]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item name="parkSelect" label="Park" rules={[{ required: true, message: 'Please select a park.' }]}>
-            {parks.length === 0 ? (
-              <Text type="secondary">There are no parks created yet!</Text>
-            ) : user?.role === StaffType.MANAGER ? (
-              <Select placeholder={getParkName(user?.parkId)} value={user?.parkId}>
-                <Select.Option key={user?.parkId} value={user?.parkId}>
-                  {getParkName(user?.parkId)}
-                </Select.Option>
-              </Select>
-            ) : (
-              <Select placeholder="Select a Park" allowClear>
-                {parks.map((park) => (
-                  <Select.Option key={park.id} value={park.id}>
-                    {park.name}
-                  </Select.Option>
-                ))}
-              </Select>
-            )}
-          </Form.Item>
-          {/* <Form.Item label="Active Status" name="activeStatus" valuePropName="checked">
-          <Switch checkedChildren="Active" unCheckedChildren="Inactive" />
-        </Form.Item> */}
-          <Form.Item wrapperCol={{ offset: 8 }}>
-            <Button type="primary" className="w-full" htmlType="submit" disabled={parks.length === 0}>
-              Submit
-            </Button>
-          </Form.Item>
-        </Form>
+        <Steps
+          current={currStep}
+          items={[
+            {
+              title: 'Details',
+              description: 'Input Staff details',
+            },
+            {
+              title: 'Complete',
+            },
+          ]}
+        />
+        {currStep === 0 && (
+          <>
+            {content[0].children}
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }} className="max-w-[600px] mx-auto">
+              <Button type="primary" className="w-full" onClick={handleSubmit} disabled={parks.length === 0}>
+                Submit
+              </Button>
+            </Form.Item>
+          </>
+        )}
+        {currStep === 1 && (
+          <Flex justify="center" className="py-4">
+            <Result
+              status="success"
+              title="Created new Staff"
+              extra={[
+                <Button key="back" onClick={() => navigate('/staff-management')}>
+                  Back to Staff Management
+                </Button>,
+                <Button type="primary" key="view" onClick={() => navigate(`/staff-management/${createdData?.id}`)}>
+                  View new Staff
+                </Button>,
+              ]}
+            />
+          </Flex>
+        )}
       </Card>
     </ContentWrapperDark>
   );
