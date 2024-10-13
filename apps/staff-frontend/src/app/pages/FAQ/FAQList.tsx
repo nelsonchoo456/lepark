147c3f5d9efcc68320229de96eee0b198c68d1c4
vveining <yongweining.ywn@gmail.com>
2024-10-13 10:04:54 +0800

    feature: view FAQ by park filtering

--- apps/staff-frontend/src/app/pages/FAQ/FAQList.tsx
+++ apps/staff-frontend/src/app/pages/FAQ/FAQList.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Tabs } from 'antd';
-import { ContentWrapperDark, LogoText } from '@lepark/common-ui';
+import { ContentWrapperDark, LogoText, useAuth } from '@lepark/common-ui';
 import PageHeader2 from '../../components/main/PageHeader2';
 import styled from 'styled-components';
 import FAQTab from './FAQTab';
 import { useFetchParks } from '../../hooks/Parks/useFetchParks';
 import { useFetchFAQs } from '../../hooks/FAQ/useFetchFAQs';
+import { StaffResponse, StaffType } from '@lepark/data-access';
 
 const TabsNoBottomMargin = styled(Tabs)`
   .ant-tabs-nav {
@@ -16,13 +17,14 @@ const TabsNoBottomMargin = styled(Tabs)`
 const FAQList: React.FC = () => {
   const { parks } = useFetchParks();
   const { faqs, triggerFetch } = useFetchFAQs();
+  const { user } = useAuth<StaffResponse>();
 
   const faqTabItems = [
-    {
+    ...(user?.role === StaffType.SUPERADMIN ? [{
       key: "all",
       label: <LogoText>All FAQs</LogoText>,
       children: <FAQTab faqs={faqs} triggerFetch={triggerFetch} showParkColumn parks={parks} />
-    },
+    }] : []),
     ...parks.map((park) => ({
       key: park.id.toString(),
       label: park.name,
