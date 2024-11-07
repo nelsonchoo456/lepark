94867d7cf93dd1872e998fd45f263430e4644c24
Aaron Foo <74005160+aaronfyr@users.noreply.github.com>
2024-11-07 16:40:41 +0800

    chore/renamed all types and api axios files to camelCase

---
+++ libs/data-access/src/lib/types/activityLog.ts
@@ -0,0 +1,44 @@
+export enum ActivityLogTypeEnum {
+  WATERED = 'WATERED',
+  TRIMMED = 'TRIMMED',
+  FERTILIZED = 'FERTILIZED',
+  PRUNED = 'PRUNED',
+  REPLANTED = 'REPLANTED',
+  CHECKED_HEALTH = 'CHECKED_HEALTH',
+  PEST_MONITORING = 'PEST_MONITORING',
+  SOIL_REPLACED = 'SOIL_REPLACED',
+  HARVESTED = 'HARVESTED',
+  STAKED = 'STAKED',
+  MULCHED = 'MULCHED',
+  MOVED = 'MOVED',
+  CHECKED = 'CHECKED',
+  ADDED_COMPOST = 'ADDED_COMPOST',
+  OTHERS = 'OTHERS',
+}
+
+export interface ActivityLogData {
+  name: string;
+  description: string;
+  dateCreated: string;
+  images?: string[];
+  activityLogType: ActivityLogTypeEnum;
+  occurrenceId: string;
+}
+
+export interface ActivityLogResponse {
+  id: string;
+  name: string;
+  description: string;
+  dateCreated: string;
+  images: string[];
+  activityLogType: ActivityLogTypeEnum;
+  occurrenceId: string;
+}
+
+export interface ActivityLogUpdateData {
+  name?: string;
+  description?: string;
+  dateCreated?: string;
+  images?: string[];
+  activityLogType?: ActivityLogTypeEnum;
+}
---
+++ libs/data-access/src/lib/types/plantTask.ts
@@ -0,0 +1,105 @@
+import { OccurrenceResponse } from './occurrence';
+import { PlantTaskStatusEnum, PlantTaskTypeEnum, PlantTaskUrgencyEnum } from './sharedEnums';
+import { StaffResponse } from './staff';
+import { ZoneResponse } from './zone';
+import { ParkResponse } from './park';
+
+export interface PlantTaskData {
+  title: string;
+  description: string;
+  taskType: PlantTaskTypeEnum;
+  taskUrgency: PlantTaskUrgencyEnum;
+  dueDate: string | null;
+  images: string[];
+  occurrenceId: string;
+  assignedStaffId?: string | null;
+  submittingStaffId: string;
+}
+
+export interface PlantTaskResponse {
+  id: string;
+  title: string;
+  description: string;
+  taskStatus: PlantTaskStatusEnum;
+  taskType: PlantTaskTypeEnum;
+  taskUrgency: PlantTaskUrgencyEnum;
+  createdAt: string;
+  updatedAt: string;
+  dueDate: string;
+  completedDate: string | null;
+  images: string[];
+  remarks: string | null;
+  occurrenceId: string;
+  occurrence: OccurrenceResponse & {
+    zone: ZoneResponse & {
+      park: ParkResponse;
+    };
+  };
+  assignedStaffId: string | null;
+  assignedStaff?: StaffResponse;
+  submittingStaffId: string;
+  submittingStaff: StaffResponse;
+  position: number; // Add this line
+}
+
+export interface PlantTaskUpdateData {
+  title?: string;
+  description?: string;
+  taskStatus?: PlantTaskStatusEnum;
+  taskType?: PlantTaskTypeEnum;
+  taskUrgency?: PlantTaskUrgencyEnum;
+  dueDate?: string | null;
+  images?: string[];
+  position?: number; // Add this line
+}
+
+export interface CompletionRateData {
+  staff: StaffResponse;
+  completionRate: number;
+}
+
+export interface OverdueRateData {
+  staff: StaffResponse;
+  overdueRate: number;
+}
+
+export interface AverageCompletionTimeData {
+  staff: StaffResponse;
+  averageCompletionTime: number;
+}
+
+export interface TaskLoadPercentageData {
+  staff: StaffResponse;
+  taskLoadPercentage: number;
+}
+
+export interface StaffPerformanceRankingData {
+  bestPerformer: StaffResponse | null;
+  secondBestPerformer: StaffResponse | null;
+  thirdBestPerformer: StaffResponse | null;
+  message?: string;
+}
+
+export interface ParkTaskCompletedData {
+  staff: StaffResponse;
+  taskCompleted: number;
+}
+export interface ParkStaffAverageCompletionTimeForPastMonthsData {
+  staff: StaffResponse;
+  averageCompletionTimes: number[];
+}
+
+export interface ParkStaffCompletionRatesForPastMonthsData {
+  staff: StaffResponse;
+  completionRates: number[];
+}
+
+export interface ParkStaffOverdueRatesForPastMonthsData {
+  staff: StaffResponse;
+  overdueRates: number[];
+}
+
+export interface ParkStaffTasksCompletedForPastMonthsData {
+  staff: StaffResponse;
+  tasksCompleted: number[];
+}
